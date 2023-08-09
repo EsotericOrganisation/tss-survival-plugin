adf356c134ee04a31d10fffbd74dcfc54c2d0a0d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 17:16:53 +0100

    📁 Initial commit.

---
+++ src/main/java/net/slqmy/tss_survival/TSSSurvivalPlugin.java
@@ -0,0 +1,10 @@
+package net.slqmy.tss_survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TSSSurvivalPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
