ce0f9670100e4059e2c1867f9bacf580c67e72fb
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-12 15:55:01 +0100

    🌍 Implemented new `Claim` class.

--- src/main/java/net/slqmy/tss_survival/command/ClaimCommand.java
+++ src/main/java/net/slqmy/tss_survival/command/ClaimCommand.java
@@ -5,21 +5,31 @@ import dev.jorel.commandapi.executors.CommandArguments;
 import net.slqmy.tss_core.datatype.Rank;
 import net.slqmy.tss_core.datatype.player.Message;
 import net.slqmy.tss_core.datatype.player.PlayerProfile;
+import net.slqmy.tss_core.datatype.player.survival.Claim;
 import net.slqmy.tss_core.datatype.player.survival.SurvivalPlayerData;
 import net.slqmy.tss_core.manager.MessageManager;
 import net.slqmy.tss_survival.TSSSurvivalPlugin;
 import org.bukkit.Chunk;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ClaimCommand {
+
   public ClaimCommand(TSSSurvivalPlugin plugin) {
 	new CommandAPICommand("claim")
 			.executesPlayer((Player player, CommandArguments args) -> {
+			  MessageManager messageManager = plugin.getCore().getMessageManager();
+			  if (!plugin.getSurvivalWorldNames().contains(player.getWorld().getName())) {
+				messageManager.sendMessage(player, Message.CANT_CLAIM_HERE);
+				return;
+			  }
+
 			  Rank playerRank = plugin.getRanksPlugin().getRankManager().getPlayerRank(player);
 
-			  MessageManager messageManager = plugin.getCore().getMessageManager();
 			  if (playerRank.getWeight() == 0) {
 				messageManager.sendMessage(player, Message.RANK_REQUIRED);
 				return;
@@ -30,19 +40,24 @@ public class ClaimCommand {
 			  SurvivalPlayerData survivalData = profile.getSurvivalData();
 			  // Check if they have enough allowed claims. If not, return.
 
-			  ArrayList<int[]> claims = survivalData.getClaims();
+			  List<Claim> claims = survivalData.getClaims();
 
 			  Chunk chunk = player.getChunk();
 			  int chunkX = chunk.getX();
 			  int chunkZ = chunk.getZ();
-			  int[] chunkCoordinates = new int[] { chunkX, chunkZ};
 
-			  if (claims.contains(chunkCoordinates)) {
+			  Claim newClaim = new Claim(player.getWorld().getName(), chunkX, chunkZ);
+
+			  if (claims.contains(newClaim)) {
 				messageManager.sendMessage(player, Message.CHUNK_ALREADY_CLAIMED);
 				return;
 			  }
 
-			  claims.add(chunkCoordinates);
+			  PersistentDataContainer container = chunk.getPersistentDataContainer();
+			  container.set(new NamespacedKey(plugin, "chunk_claim_owner"), PersistentDataType.STRING, player.getUniqueId().toString());
+
+			  claims.add(newClaim);
+
 			  messageManager.sendMessage(player, Message.CHUNK_CLAIMED_SUCCESSFULLY);
 			})
 			.register();
