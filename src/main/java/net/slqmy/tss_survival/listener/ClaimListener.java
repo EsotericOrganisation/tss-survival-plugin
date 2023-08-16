6314d3906ab7ba326233388cfbec85dff2a8c5c5
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-16 01:50:31 +0100

    ⛏ Added more types of interaction prevention in claimed chunks.

--- src/main/java/net/slqmy/tss_survival/listener/ClaimListener.java
+++ src/main/java/net/slqmy/tss_survival/listener/ClaimListener.java
@@ -1,14 +1,21 @@
 package net.slqmy.tss_survival.listener;
 
 import net.slqmy.tss_core.datatype.player.Message;
+import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_survival.TSSSurvivalPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.block.data.Openable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
@@ -23,21 +30,57 @@ public class ClaimListener implements Listener {
 	this.plugin = plugin;
   }
 
+  @EventHandler
+  public void onClaimInteract(@NotNull PlayerInteractEvent event) {
+	Chunk chunk = null;
+	Player player = event.getPlayer();
+
+	switch (event.getAction()) {
+	  case LEFT_CLICK_AIR, RIGHT_CLICK_AIR, LEFT_CLICK_BLOCK -> {
+		return;
+	  }
+	  case RIGHT_CLICK_BLOCK -> {
+		Block block = event.getClickedBlock();
+		DebugUtil.log("IS CONTAINER? " + (block.getState() instanceof Container));
+		DebugUtil.log("IS OPENABLE? " + (block.getState() instanceof Openable));
+		DebugUtil.log("BLOCK: " + (block.getState()));
+		DebugUtil.log("BLOCK CLASS: " + (block.getState().getClass()));
+
+		if (!(block.getState() instanceof Container)) {
+		  return;
+		}
+
+		chunk = block.getChunk();
+	  }
+	  case PHYSICAL -> chunk = event.getPlayer().getChunk();
+	}
+
+	event.setCancelled(handleClaimedChunkInteract(chunk, player));
+  }
+
   @EventHandler(priority = EventPriority.LOW)
-  public void onClaimBlockBreak(@NotNull BlockBreakEvent event) {
-	Chunk chunk = event.getBlock().getChunk();
+  public void onClaimedBlockBreak(@NotNull BlockBreakEvent event) {
+	event.setCancelled(handleClaimedChunkInteract(event.getBlock().getChunk(), event.getPlayer()));
+  }
+
+  @EventHandler
+  public void onClaimedChunkBlockPlace(BlockPlaceEvent event) {
+	event.setCancelled(handleClaimedChunkInteract(event.getBlock().getChunk(), event.getPlayer()));
+  }
+
+  private boolean handleClaimedChunkInteract(@NotNull Chunk chunk, Player player) {
 	PersistentDataContainer container = chunk.getPersistentDataContainer();
 	String chunkOwnerUuidString = container.get(new NamespacedKey(plugin, "chunk_claim_owner"), PersistentDataType.STRING);
 
 	if (chunkOwnerUuidString == null) {
-	  return;
+	  return false;
 	}
-
-	Player player = event.getPlayer();
 
 	if (!UUID.fromString(chunkOwnerUuidString).equals(player.getUniqueId())) {
-	  event.setCancelled(true);
-	  plugin.getCore().getMessageManager().sendMessage(player, Message.CANT_INTERACT_BECAUSE_CHUNK_CLAIMED);
+	  plugin.getCore().getMessageManager().sendMessage(player, Message.CANT_INTERACT_BECAUSE_CHUNK_CLAIMED, Bukkit.getOfflinePlayer(UUID.fromString(chunkOwnerUuidString)).getName());
+	  return true;
 	}
+
+	return false;
   }
 }
