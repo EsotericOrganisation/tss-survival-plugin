c40715bdddbcdce9f00ec04166291bc449230af6
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-19 02:26:11 +0100

    📋 Registered the `/give-claim-map` command.

--- src/main/java/net/slqmy/tss_survival/TSSSurvivalPlugin.java
+++ src/main/java/net/slqmy/tss_survival/TSSSurvivalPlugin.java
@@ -2,8 +2,16 @@ package net.slqmy.tss_survival;
 
 import net.slqmy.tss_core.TSSCorePlugin;
 import net.slqmy.tss_ranks.TSSRanksPlugin;
-import net.slqmy.tss_survival.command.*;
-import net.slqmy.tss_survival.listener.*;
+import net.slqmy.tss_survival.command.SkillsCommand;
+import net.slqmy.tss_survival.command.TradeCommand;
+import net.slqmy.tss_survival.command.claim.*;
+import net.slqmy.tss_survival.listener.BlockListener;
+import net.slqmy.tss_survival.listener.OreMineListener;
+import net.slqmy.tss_survival.listener.SurvivalConnectionListener;
+import net.slqmy.tss_survival.listener.TradeListener;
+import net.slqmy.tss_survival.listener.claim.ClaimListener;
+import net.slqmy.tss_survival.listener.skill.SkillsListener;
+import net.slqmy.tss_survival.listener.skill.SkillsMenuListener;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
@@ -34,6 +42,7 @@ public final class TSSSurvivalPlugin extends JavaPlugin {
 	new UnClaimCommand(this);
 
 	new ClaimMapCommand(this);
+	new GiveClaimMapCommand(this);
 
 	new TrustCommand(this);
 	new UnTrustCommand(this);
